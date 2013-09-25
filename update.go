13135d123342906231771dbb0546def66657351f
David Dollar <ddollar@gmail.com>
2013-09-25 11:17:21 -0400

    dist from heroku/force

--- update.go
+++ update.go
@@ -26,7 +26,7 @@ func runUpdate(cmd *Command, args []string) {
 		fmt.Println("ERROR: can't update dev version")
 		return
 	}
-	d := dist.NewDist("ddollar/force", Version)
+	d := dist.NewDist("heroku/force", Version)
 	to, err := d.Update()
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err)
