c0f559b68337a27e45d566d2f031664a223b6773
David Dollar <ddollar@gmail.com>
2013-09-29 11:20:01 -0400

    use stderr for errors

--- error.go
+++ error.go
@@ -11,9 +11,9 @@ const (
 
 func ErrorAndExit(format string, args ...interface{}) {
 	if format[0] == LF {
-		fmt.Printf(format[1:]+"\n", args...)
+		fmt.Errorf(format[1:]+"\n", args...)
 	} else {
-		fmt.Printf(fmt.Sprintf("ERROR: %s\n", format), args...)
+		fmt.Errorf(fmt.Sprintf("ERROR: %s\n", format), args...)
 	}
 	os.Exit(1)
 }
