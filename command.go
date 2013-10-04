307d9ab4fa75193c59f571ee69e58aee6e03feba
quintonwall <qwall@salesforce.com>
2013-10-04 09:13:00 -0700

    changed forego to force for output

--- command.go
+++ command.go
@@ -21,7 +21,7 @@ type Command struct {
 
 func (c *Command) printUsage() {
 	if c.Runnable() {
-		fmt.Printf("Usage: forego %s\n\n", c.Usage)
+		fmt.Printf("Usage: force %s\n\n", c.Usage)
 	}
 	fmt.Println(strings.Trim(c.Long, "\n"))
 }
