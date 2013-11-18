30d84256662931f80bb9316b9e5d21e3ea91c23b
Dave Carroll <djcarroll@gmail.com>
2013-11-17 16:03:23 -0800

    fix about me

--- whoami.go
+++ whoami.go
@@ -31,7 +31,11 @@ func runWhoami(cmd *Command, args []string) {
 		if err != nil {
 			ErrorAndExit(err.Error())
 		} else {
-			fmt.Printf("\nAbout %s\n\nOn twitter: @dcarroll\n\n%s\n\n", records[0]["Name"], records[0]["AboutMe"].(string))
+			if records[0]["AboutMe"] != nil {
+				fmt.Printf("\nAbout %s\n\nOn twitter: @dcarroll\n\n%s\n\n", records[0]["Name"], records[0]["AboutMe"])
+			} else {
+				fmt.Printf("\nAbout %s\n\nOn twitter: @dcarroll\n\n", records[0]["Name"])
+			}
 		}	
 	}
 }
