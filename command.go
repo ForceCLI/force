31aa6972eda291545e03255e2ed7ebbbba700aa7
lpoulain <laurent.poulain@gmail.com>
2013-11-19 15:49:28 -0500

    Added extra options for 'force export' (export only one type along with profiles)

--- command.go
+++ command.go
@@ -21,7 +21,7 @@ type Command struct {
 
 func (c *Command) printUsage() {
 	if c.Runnable() {
-		fmt.Printf("Usage (2): force %s\n\n", c.Usage)
+		fmt.Printf("Usage: force %s\n\n", c.Usage)
 	}
 	fmt.Println(strings.Trim(c.Long, "\n"))
 }
