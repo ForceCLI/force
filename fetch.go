b2f480ee775d5345695d968c48e0ddebcf6abb45
Dave Carroll <djcarroll@gmail.com>
2013-12-02 09:57:58 -0800

    merge upstream

---
+++ fetch.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+var cmdFetch = &Command{
+	Run: runFetch,
+	Usage: "fetch <type> [<artifact name>]",
+	Short: "Export specified artifact(s) to a local directory",
+	Long: `
+Export specified artifact(s) to a local directory
+
+Examples
+
+  force fetch CustomObject Book__c Author__c
+`,
+}
+
+func runFetch(cmd *Command, args []string) {
+	wd, _ := os.Getwd()
+	root := filepath.Join(wd, "metadata")
+	if len(args) < 1 {
+		ErrorAndExit("must specify object type and/or object name")
+	}
+
+	artifactType := args[0]
+	query := ForceMetadataQuery{}
+	if len(args) >= 2 {
+		newargs := args[1:]
+		for artifactNames := range newargs {
+			mq := ForceMetadataQueryElement{artifactType, newargs[artifactNames]}
+			query = append(query, mq)
+		}
+	} else {
+		mq := ForceMetadataQueryElement{artifactType, "*"}
+		query = append(query, mq)
+	}
+
+	force, _ := ActiveForce()
+	files, err := force.Metadata.Retrieve(query)
+	if err != nil {
+		ErrorAndExit(err.Error())
+	}
+	for name, data := range files {
+		file := filepath.Join(root, name)
+		dir := filepath.Dir(file)
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			ErrorAndExit(err.Error())
+		}
+		if err := ioutil.WriteFile(filepath.Join(root, name), data, 0644); err != nil {
+			ErrorAndExit(err.Error())
+		}
+	}
+	fmt.Printf("Exported to %s\n", root)
+}
--- whoami.go
+++ whoami.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 )
 
 var cmdWhoami = &Command{
@@ -25,17 +23,5 @@ func runWhoami(cmd *Command, args []string) {
 		ErrorAndExit(err.Error())
 	} else if len(args) == 0 {
 		DisplayForceRecord(me)
-	} else {
-		parts := strings.Split(force.Credentials.Id, "/")
-		records, err := force.Query(fmt.Sprintf("select Name, AboutMe From User Where Id = '%s'", parts[len(parts)-1]))
-		if err != nil {
-			ErrorAndExit(err.Error())
-		} else {
-			if records[0]["AboutMe"] != nil {
-				fmt.Printf("\nAbout %s\n\nOn twitter: @dcarroll\n\n%s\n\n", records[0]["Name"], records[0]["AboutMe"])
-			} else {
-				fmt.Printf("\nAbout %s\n\nOn twitter: @dcarroll\n\n", records[0]["Name"])
-			}
-		}	
-	}
+	} 
 }
