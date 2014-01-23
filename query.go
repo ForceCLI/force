fc0f81aa6066f65af9d570716f587f4951046a25
Dave Carroll <djcarroll@gmail.com>
2014-01-22 16:34:22 -0800

    Fetch, query and import additions
    fetch.go - updated usage to indicate package access
    logins.go - change message from "no accounts" to "no logins"
    partner.go - added soapExecuteCore for core SOAP api namespace
    accomodation.
    query.go - added support for output format specification
    sobject.go - added import function to import new data

--- query.go
+++ query.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 )
 
 var cmdQuery = &Command{
 	Run:   runQuery,
-	Usage: "query <soql>",
-	Short: "Execute a SOQL query",
+	Usage: "query <soql> [format:<json, csv>]",
+	Short: "Execute a SOQL query, optionally specify output format",
 	Long: `
 Execute a SOQL query
 
 Examples:
 
   force query select id, name from user
+
+  force query select Id, FirstName, LastName From Contact format:json
 `,
 }
 
@@ -22,11 +26,24 @@ func runQuery(cmd *Command, args []string) {
 	if len(args) < 1 {
 		ErrorAndExit("must specify a query")
 	}
-	query := strings.Join(args, " ")
+	var query = ""
+	if args[len(args)-1] == "format:json" {
+		query = strings.Join(args[:len(args)-1], " ")
+	} else {
+		query = strings.Join(args, " ")
+	}
 	records, err := force.Query(query)
 	if err != nil {
 		ErrorAndExit(err.Error())
 	} else {
-		DisplayForceRecords(records)
+		if args[len(args)-1] == "format:json" {
+			d, err := json.MarshalIndent(records, "", " ")
+			if err != nil {
+				ErrorAndExit(err.Error())
+			}
+			fmt.Println(string(d))
+		} else {
+			DisplayForceRecords(records)
+		}
 	}
 }
