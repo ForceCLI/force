5360fff3a280fedd48b23196a624c1b6ba8f6515
agn-fj <furniss_justin@allergan.com>
2015-03-19 21:58:15 -0700

    go fmt
    nice!

--- limits.go
+++ limits.go
@@ -24,23 +24,22 @@ func runLimits(cmd *Command, args []string) {
 
 	force, _ := ActiveForce()
 
-		var result ForceLimits
-		result, err := force.GetLimits()
+	var result ForceLimits
+	result, err := force.GetLimits()
 
-		if err != nil {
-			ErrorAndExit(err.Error())
-		} else {
-			printLimits(result)
-		}
+	if err != nil {
+		ErrorAndExit(err.Error())
+	} else {
+		printLimits(result)
+	}
 }
 
-
-func printLimits(result map[string]ForceLimit)() {
+func printLimits(result map[string]ForceLimit) {
 
 	//sort keys
 	var keys []string
 	for k := range result {
-			keys = append(keys, k)
+		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 
