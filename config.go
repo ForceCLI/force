0077b47e9d90daa0b16d9363640eada7f01f0ab6
Dave Carroll <djcarroll+git@gmail.com>
2015-06-26 08:20:11 -0700

    fix push double folder

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ import (
 
 var Config = config.NewConfig("force")
 
-func GetSourceDir(bdir string) (src string, err error) {
+func GetSourceDir() (src string, err error) {
 	// Last element is default
 	var sourceDirs = []string{
 		//"src",
@@ -19,11 +19,6 @@ func GetSourceDir(bdir string) (src string, err error) {
 
 	wd, err := os.Getwd()
 
-	/*if len(bdir) != 0 {
-		wd = bdir
-		fmt.Println("BDir: ", bdir)
-		os.Chdir(bdir)
-	}*/
 	err = nil
 	for _, src = range sourceDirs {
 		if strings.Contains(wd, src) {
