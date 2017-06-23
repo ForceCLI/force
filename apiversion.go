1b30b88d33a5e0f9482122974bf125789271fac4
Dave Carroll <djcarroll@gmail.com>
2017-06-23 08:53:20 -0700

    Update apiversion.go

--- apiversion.go
+++ apiversion.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-var apiVersionNumber = "39.0"
+var apiVersionNumber = "40.0"
 var apiVersion = fmt.Sprintf("v%s", apiVersionNumber)
 
 var cmdApiVersion = &Command{
@@ -18,7 +18,7 @@ Display/Set current API version
 Examples:
 
   force apiversion
-  force apiversion 39.0
+  force apiversion 40.0
 `,
 }
 
