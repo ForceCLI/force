c0044cc1c7b9973b531e157e821e3194dbb1fb35
Hans Alves <halves-git@qzwx.nl>
2018-05-12 13:52:51 +0200

    Allow creation of triggers and VF components

--- command/create.go
+++ command/create.go
@@ -18,17 +18,26 @@ Examples (both flags are required):
 
   force create -w ApexClass -n NewController
 
+  force create -w ApexTrigger -n NewTrigger -s Account
+
   force create -w ApexPage -n CoolPage
+
+  force create -w ApexComponent -n CoolComponent
 `,
 }
 var (
 	what     string
+	sObjectName  string
 	itemName string
 )
 
 func init() {
 	cmdCreate.Flag.StringVar(&what, "what", "", "What type of thing to create (currently only Apex or Visualforce).")
 	cmdCreate.Flag.StringVar(&what, "w", "", "What type of thing to create (currently only Apex or Visualforce).")
+	cmdCreate.Flag.StringVar(&what, "type", "", "What type of thing to create (currently only Apex or Visualforce).") // Because consistency with other commands like fetch
+	cmdCreate.Flag.StringVar(&what, "t", "", "What type of thing to create (currently only Apex or Visualforce).")
+	cmdCreate.Flag.StringVar(&sObjectName, "sobject", "", "For which sobject should the thing be created (only for Triggers).")
+	cmdCreate.Flag.StringVar(&sObjectName, "s", "", "For which sobject should the thing be created (only for Triggers).")
 	cmdCreate.Flag.StringVar(&itemName, "n", "", "Name of thing to be created.")
 	cmdCreate.Flag.StringVar(&itemName, "name", "", "Name of thing to be created.")
 	cmdCreate.Run = runCreate
@@ -43,6 +52,14 @@ func runCreate(cmd *Command, args []string) {
 		switch strings.ToLower(what) {
 		case "apexclass":
 			attrs = getApexDefinition()
+		case "apextrigger":
+			if len(sObjectName) == 0 {
+				cmd.PrintUsage()
+				return
+			}
+			attrs = getTriggerDefinition()
+		case "apexcomponent":
+			attrs = getVFComponentDefinition()
 		case "visualforce", "apexpage":
 			what = "apexpage"
 			attrs = getVFDefinition()
@@ -59,7 +76,6 @@ func runCreate(cmd *Command, args []string) {
 }
 
 func getVFDefinition() (attrs map[string]string) {
-	//{ "markup": "<apex:page> </apex:page>", "name": "mytestpage", "masterlabel": "Test Page" }
 	attrs = make(map[string]string)
 	attrs["markup"] = "<apex:page>\n\n</apex:page>"
 	attrs["name"] = itemName
@@ -67,12 +83,28 @@ func getVFDefinition() (attrs map[string]string) {
 	return
 }
 
+func getVFComponentDefinition() (attrs map[string]string) {
+	attrs = make(map[string]string)
+	attrs["markup"] = "<apex:component>\n\n</apex:component>"
+	attrs["name"] = itemName
+	attrs["masterlabel"] = strings.Replace(itemName, " ", "_", -1)
+	return
+}
+
 func getApexDefinition() (attrs map[string]string) {
-	//{ "markup": "<apex:page> </apex:page>", "name": "mytestpage", "masterlabel": "Test Page" }
 	attrs = make(map[string]string)
 	attrs["status"] = "Active"
 	attrs["body"] = fmt.Sprintf("public with sharing class %s {\n\n}", itemName)
 	attrs["name"] = itemName
+	return
+}
+
+func getTriggerDefinition() (attrs map[string]string) {
+	//{ "markup": "<apex:page> </apex:page>", "name": "mytestpage", "masterlabel": "Test Page" }
+	attrs = make(map[string]string)
+	attrs["status"] = "Active"
+	attrs["body"] = fmt.Sprintf("trigger %s on %s (before insert, after insert, before update, after update, before delete, after delete, after undelete) { \n\n }", itemName, sObjectName)
 	attrs["name"] = itemName
+	attrs["TableEnumOrId"] = sObjectName
 	return
 }
