e1b0a5031ad388743c9a2ef08001fbce91ec6d8c
Christian G. Warden <cwarden@xerus.org>
2022-11-02 09:56:23 -0500

    Display Remaining Limit In Red If Below Warning Threshold

--- command/limits.go
+++ command/limits.go
@@ -15,35 +15,38 @@ var limitsCmd = &cobra.Command{
 	Use:   "limits",
 	Short: "Display current limits",
 	Long: `
-	Use the limits command to display limits information for your organization.
+Use the limits command to display limits information for your organization.
 
-	 -- Max is the limit total for the organization.
+-- Max is the limit total for the organization.
 
-	 -- Remaining is the total number of calls or events left for the organization.`,
+-- Remaining is the total number of calls or events left for the organization.`,
 	Args: cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		runLimits()
+		warn, _ := cmd.Flags().GetFloat64("warn")
+		runLimits(warn)
 	},
 }
 
 func init() {
+	limitsCmd.Flags().Float64P("warn", "w", 10, "warning percentange.  highlight if remaining is less.")
 	RootCmd.AddCommand(limitsCmd)
 }
 
-func runLimits() {
+func runLimits(warn float64) {
 	var result ForceLimits
 	result, err := force.GetLimits()
 
 	if err != nil {
 		ErrorAndExit(err.Error())
 	} else {
-		printLimits(result)
+		printLimits(result, warn)
 	}
 }
 
-func printLimits(result map[string]ForceLimit) {
+func printLimits(result map[string]ForceLimit, warnPercent float64) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Limit", "Maximum", "Remaining"})
+	table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_RIGHT, tablewriter.ALIGN_RIGHT})
 	//sort keys
 	var keys []string
 	for k := range result {
@@ -52,7 +55,14 @@ func printLimits(result map[string]ForceLimit) {
 	sort.Strings(keys)
 
 	for _, k := range keys {
-		table.Append([]string{k, strconv.FormatInt(result[k].Max, 10), strconv.FormatInt(result[k].Remaining, 10)})
+		remaining := result[k].Remaining
+		max := result[k].Max
+		row := []string{k, strconv.FormatInt(max, 10), strconv.FormatInt(remaining, 10)}
+		if max > 0 && float64(remaining)/float64(max) < warnPercent/100 {
+			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{}, tablewriter.Colors{tablewriter.BgRedColor}})
+		}
+
+		table.Append(row)
 	}
 	if table.NumLines() > 0 {
 		table.Render()
